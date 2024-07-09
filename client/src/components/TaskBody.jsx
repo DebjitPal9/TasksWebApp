98d2c6385f4d81fd954e5f459b279f2f9e8e2aa8
Debjit Pal <63508896+DebjitPal9@users.noreply.github.com>
2024-07-09 22:47:28 +0530

    Footer edited

---
+++ client/src/components/TaskBody.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TaskBody = () => {
+  return (
+    <div className='bg-gray-700'>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto eveniet nulla blanditiis recusandae totam quod omnis odit, eos reiciendis distinctio aliquid! Ratione fuga architecto esse adipisci consequatur incidunt delectus enim. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo adipisci ipsam nostrum? Hic sit modi aliquid perferendis, vitae beatae pariatur repudiandae! Nobis neque cumque repudiandae numquam earum, officiis nulla deserunt.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, deleniti perferendis est sequi molestias iusto perspiciatis assumenda suscipit debitis. Maxime nemo fuga mollitia magnam aperiam harum porro laboriosam libero dignissimos.
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam veniam libero omnis eveniet rem, laudantium reiciendis perferendis magni itaque soluta earum blanditiis nesciunt ea ad assumenda laboriosam dolore, laborum eum!
+    </div>
+  )
+}
+
+export default TaskBody
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBarTop from '../components/NavBarTop'
-
+import Footer from '../components/Footer'
+import TaskBody from '../components/TaskBody'
 
 export default function HomePage() {
   return (
     <>
     <NavBarTop></NavBarTop>
-    
+    <TaskBody></TaskBody>
+    <Footer></Footer>
     </>
   )
 }
